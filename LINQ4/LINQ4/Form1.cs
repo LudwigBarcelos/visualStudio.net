f5da56fc60b80db910443024eab6f20647e3179d
Ludwig Barcelos <111275796+LudwigBarcelos@users.noreply.github.com>
2022-08-16 07:39:09 -0500

    pract4

---
+++ LINQ4/LINQ4/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LINQ4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<Trabajador> listaTrabajadores;
+            private void button1_Click(object sender, EventArgs e)
+        {
+            listaTrabajadores = new List<Trabajador>();
+            listaTrabajadores.Add(new Trabajador { Id = Convert.ToInt32(textBox1.Text), Nombre = textBox2.Text, ApellidoPat = textBox3.Text, ApellidoMat = textBox4.Text, Sueldo = Convert.ToInt32(textBox5.Text), Puesto= comboBox1.SelectedItem.ToString() });
+            llenaText();
+        }
+
+        private void llenaText()
+        {
+            IEnumerable<Trabajador> t1 = from Trabajador in listaTrabajadores orderby Trabajador.Nombre select Trabajador;
+            foreach (Trabajador trabaja in t1)
+            {
+                textBox6.AppendText(Convert.ToString(trabaja.getId()));
+                textBox6.AppendText(" "+trabaja.getNombre());
+                textBox6.AppendText(" " + trabaja.getApellidoPat());
+                textBox6.AppendText(" " + trabaja.getApellidoMat());
+                textBox6.AppendText(" "+Convert.ToString(trabaja.getSueldo()));
+                textBox6.AppendText(" " + trabaja.getPuesto());
+                textBox6.AppendText(Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ LINQ4/LINQ4/Trabajador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ4
+{
+    public class Trabajador
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPat { get; set; }
+        public string ApellidoMat { get; set; }
+        public int Sueldo { get; set; }
+        public string Puesto { get; set; }
+        public int getId()
+        {
+            return Id;
+        }
+        public string getNombre()
+        {
+            return Nombre;
+        }        
+        public string getApellidoPat()
+        {
+            return ApellidoPat;
+        }
+        public string getApellidoMat()
+        {
+            return ApellidoMat;
+        }
+        public int getSueldo()
+        {
+            return Id;
+        }
+        public string getPuesto()
+        {
+            return Puesto;
+        }
+        public string getDatos(int id)
+        {
+            string Datos = Convert.ToString("Id:{0} ");
+                //"Nombre:{1} ApellidoPaterno:{2} ApellidoMaterno:{3} Sueldo:{4} Puesto:{5} ",Id,Nombre,ApellidoPat,ApellidoMat,Sueldo,Puesto);
+            //Console.WriteLine("Id:{0} Nombre:{1} ApellidoPaterno:{2} ApellidoMaterno:{3} Sueldo:{4} Puesto:{5} ", Id, Nombre, ApellidoPat, ApellidoMat, Sueldo, Puesto);
+           return Datos;
+        }
+    }
+}
